13654552a7eb0c769cb0ed510cb0eca86ab7606b
kk <405437445@qq.com>
2026-03-13 11:25:10 +0800

    refactor: remove Win32 native UI, keep ImGui version only

--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "mainwindow.h"
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MainWindow window(hInstance);
-    return window.Run();
-}
